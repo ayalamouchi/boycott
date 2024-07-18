81c66b9743ec10cea62d8d94f076fbbbd6bf9d1a
ayalamouchi <ayalamouchi6@gmail.com>
2024-07-18 17:59:41 +0100

    signup

--- ListCategory/index.php
+++ ListCategory/index.php
@@ -1,7 +1,13 @@
-<?php 
+<?php
+require_once ('../db_connect/index.php');
+$categories = $pdo->prepare("select * from categories");
+$categories->execute();
+$res = $categories->fetchAll();
 
- $page_titel="ListCategory";
- $template="ListCategory";
- include "../layout.phtml";
- 
- ?>
+
+
+$page_titel = "ListCategory";
+$template = "ListCategory";
+include "../layout.phtml";
+
+?>
